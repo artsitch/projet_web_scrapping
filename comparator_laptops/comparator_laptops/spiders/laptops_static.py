2bc9104c27a2d9dc76459ed911b7acd76354845c
arthur <arthur.sitcheping@groupe-esigelec.org>
2026-01-07 00:25:25 +0100

    scrapping du deuxieme site tenant compte de la pagination

--- comparator_laptops/comparator_laptops/spiders/laptops_static.py
+++ comparator_laptops/comparator_laptops/spiders/laptops_static.py
@@ -3,29 +3,26 @@ from ..items import LaptopsItem
 
 
 class LaptopsSpider(scrapy.Spider):
-    name = "laptops"
+    name = "laptops_static"
     allowed_domains = ["webscraper.io"]
-    start_urls = ["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"]
+    start_urls = ["https://webscraper.io/test-sites/e-commerce/static/computers/laptops"]
 
     def parse(self, response):
         products = response.xpath('//div[contains(@class,"thumbnail")]')
-        print(f"Nombre de produits trouvé : {len(products)}")
-        #self.logger.info(f"Nombre de produits trouvé : {len(products)}"), equivalent du print mieux gere par scrapy
+        self.logger.info(f"Nombre de produits trouvé : {len(products)}")
 
-
-        for prod in products :
+        for prod in products:
             title = prod.xpath('.//a[contains(@class, "title")]/text()').get()
             price = prod.xpath('.//span[@itemprop="price"]/text()').get()
             product_url = prod.xpath('.//a[contains(@class, "title")]/@href').get()
-            image_url = prod.xpath('.//img[contains(@class, "image")]/@src').get()
-            # recuperation des infos sur les produits
-            laptop = LaptopsItem()
+            image_url = prod.xpath('.//img/@src').get()
 
             if title:
                 title = title.strip()
             if price:
                 price = price.strip()
-            
+
+            laptop = LaptopsItem()
             laptop["name"] = title
             laptop["price"] = price
             laptop["product_url"] = response.urljoin(product_url)
@@ -33,3 +30,9 @@ class LaptopsSpider(scrapy.Spider):
 
             yield laptop
 
+        # Pagination : initialiser next_page AVANT la boucle
+        next_page = response.xpath('//a[normalize-space(text())="›"]/@href').get()
+
+        if next_page is not None:
+            self.logger.info(f"Next page URL: {next_page}")
+            yield response.follow(next_page, callback=self.parse)
